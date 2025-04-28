97601948612e6892282ec3971b879b072e5e26f6
Ravi Kumar Patel <ravikumarpatel2001@gmail.com>
2025-04-28 15:07:38 +0530

    initial project setup

---
+++ src/test/java/com/salon/project/SalonManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salon.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalonManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
